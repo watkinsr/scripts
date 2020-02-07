f3531854631d6d1740df1a21ec45f98982b3eb69
watkinsr <ryanwatkins54@gmail.com>
2020-02-07 12:43:43 +0000

    sync mail don't notify

--- sync-mail.sh
+++ sync-mail.sh
@@ -3,6 +3,6 @@
 # Sync Mail
 
 export DISPLAY=:0
-/usr/bin/notify-send "Sync mail job [BEGIN]";
+# /usr/bin/notify-send "Sync mail job [BEGIN]";
 /usr/bin/offlineimap;
-/usr/bin/notify-send "Sync mail job [FINISH]";
+# /usr/bin/notify-send "Sync mail job [FINISH]";
