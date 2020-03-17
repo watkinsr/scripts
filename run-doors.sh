496cf7a1eb313c1ee3ff1f573706ee4e1a61664e
watkinsr <ryanwatkins54@gmail.com>
2020-03-17 10:28:38 +0000

    Fix menu bug

--- run-doors.sh
+++ run-doors.sh
@@ -1,11 +1,12 @@
-#Xephyr :3 -resizeable -screen 1366x768 &
+# Xephyr :3 -resizeable -screen 1366x768 &
 
 
-#DISPLAY=:3 sh ~/scripts/input.sh
+# DISPLAY=:3 sh ~/scripts/input.sh &
 
-#DISPLAY=:3
+# DISPLAY=:3
 sbcl --control-stack-size 1024 \
      --dynamic-space-size 1024 \
      --eval '(ql:quickload :doors)' \
      --eval '(use-package :doors)' \
+     --eval '(defmethod clim:sheet-native-transformation ((x null)) climi::+identity-transformation+)' \
      --eval '(doors:doors)'
