2093fbd61a9859eea107e201553f5d757141124f
watkinsr <ryanwatkins54@gmail.com>
2020-03-17 08:47:18 +0000

    simplify X

---
+++ input.sh
@@ -0,0 +1,20 @@
+setxkbmap -model 'pc104caw-zqu' -layout 'us(cmk_ed_us)' -option 'misc:extend,lv5:caps_switch_lock,grp:shifts_toggle,compose:menu,misc:cmk_curl_dh'
+
+# doas bash ~/Git/Projects/BigBagKbdTrixXKB/install-dreymar-xmod.sh -ox 4a us us
+# setxkbmap -option caps:escape -v us -variant colemak;
+xset r rate 300 50 &
+
+
+# Increase mouse speed
+xinput set-prop 12 165 2.400000, 0.000000, 0.000000, 0.000000, 2.400000, 0.000000, 0.000000, 0.000000, 1.000000
+
+# Remove cursor while typing
+# xbanish &
+
+# Change X shaped default cursor
+
+# The default X shaped Xcursor appears in window managers that do not set the default cursor to
+# left_ptr or in window managers using XCB (like awesome) instead of Xlib.
+
+# To fix this simply add the following to your ~/.xinitrc , xsession or window managers startup configuration if possible (for example bspwm's bspwmrc).
+xsetroot -cursor_name left_ptr
--- run-doors.sh
+++ run-doors.sh
@@ -1,5 +1,11 @@
-sbcl --control-stack-size 200 \
+#Xephyr :3 -resizeable -screen 1366x768 &
+
+
+#DISPLAY=:3 sh ~/scripts/input.sh
+
+#DISPLAY=:3
+sbcl --control-stack-size 1024 \
+     --dynamic-space-size 1024 \
      --eval '(ql:quickload :doors)' \
-     --eval '(defun climi::compile-time-clos-p (name) nil)' \
      --eval '(use-package :doors)' \
-     --eval '(doors:doors :new-process t)'
+     --eval '(doors:doors)'
