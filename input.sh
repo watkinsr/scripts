54a4f2f80ec70367e6b8c406ac086f684c069d2d
watkinsr <ryanwatkins54@gmail.com>
2020-03-20 17:13:32 +0000

    nullity input (for bugs)

--- input.sh
+++ input.sh
@@ -1,12 +1,12 @@
-setxkbmap -model 'pc104caw-zqu' -layout 'us(cmk_ed_us)' -option 'misc:extend,lv5:caps_switch_lock,grp:shifts_toggle,compose:menu,misc:cmk_curl_dh'
+# setxkbmap -model 'pc104caw-zqu' -layout 'us(cmk_ed_us)' -option 'misc:extend,lv5:caps_switch_lock,grp:shifts_toggle,compose:menu,misc:cmk_curl_dh'
 
 # doas bash ~/Git/Projects/BigBagKbdTrixXKB/install-dreymar-xmod.sh -ox 4a us us
 # setxkbmap -option caps:escape -v us -variant colemak;
-xset r rate 300 50 &
+# xset r rate 300 50 &
 
 
 # Increase mouse speed
-xinput set-prop 12 165 2.400000, 0.000000, 0.000000, 0.000000, 2.400000, 0.000000, 0.000000, 0.000000, 1.000000
+# xinput set-prop 12 165 2.400000, 0.000000, 0.000000, 0.000000, 2.400000, 0.000000, 0.000000, 0.000000, 1.000000
 
 # Remove cursor while typing
 # xbanish &
@@ -17,4 +17,4 @@ xinput set-prop 12 165 2.400000, 0.000000, 0.000000, 0.000000, 2.400000, 0.00000
 # left_ptr or in window managers using XCB (like awesome) instead of Xlib.
 
 # To fix this simply add the following to your ~/.xinitrc , xsession or window managers startup configuration if possible (for example bspwm's bspwmrc).
-xsetroot -cursor_name left_ptr
+# xsetroot -cursor_name left_ptr
